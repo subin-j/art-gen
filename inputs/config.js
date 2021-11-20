c0d9fca4044099cd3c1d7d62ae42f6ae88829b9c
jadyJeong <coldfeet0816@gmail.com>
2021-11-20 13:30:38 +0000

    first commit

---
+++ inputs/config.js
@@ -0,0 +1,76 @@
+const fs = require("fs");
+const width = 1000;
+const height = 1000;
+const dir = __dirname;
+const rarity = [
+  { key: "", val: "original" },
+  { key: "_r", val: "rare" },
+  { key: "_sr", val: "super rate" },
+];
+
+console.log(dir);
+
+const addRarity = (_str) => {
+  let itemRarity;
+
+  rarity.forEach((r) => {
+    if (_str.includes(r.key)) {
+      itemRarity = r.val;
+    }
+  });
+
+  return itemRarity;
+};
+
+const cleanName = (_str) => {
+  let name = _str.slice(0, -4);
+  rarity.forEach((r) => {
+    name = name.replace(r.key, "");
+  });
+  return name;
+};
+
+const getElements = (path) => {
+  return fs
+    .readdirSync(path)
+    .filter((item) => !/(^|\/)\.[^\/\.]/g.test(item))
+    .map((i, index) => {
+      return {
+        id: index + 1,
+        name: cleanName(i),
+        fileName: i,
+        rarity: addRarity(i),
+      };
+    });
+};
+
+const layers = [
+  {
+    id: 1,
+    name: "background",
+    location: `${dir}/background/`,
+    elements: getElements(`${dir}/background/`),
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 2,
+    name: "feature_A",
+    location: `${dir}/feature_A/`,
+    elements: getElements(`${dir}/feature_A/`),
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+  {
+    id: 3,
+    name: "feature_B",
+    location: `${dir}/feature_B/`,
+    elements: getElements(`${dir}/feature_B/`),
+    position: { x: 0, y: 0 },
+    size: { width: width, height: height },
+  },
+];
+
+console.log(layers);
+
+module.exports = { layers, width, height };
